2fb5a2acd6cc38ce95edbe9a7df6ccc2f76ddb18
caion90 <105600461+caion90@users.noreply.github.com>
2022-10-09 14:27:17 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -29,15 +29,9 @@ int dimmer_pin[] = {14, 5, 15};
 
 void callback(char *topic, byte *payload, unsigned int length)
 {
-  // log("Message arrived");
-  // log(topic[0]);
-  // log((char)payload[0]);
-  // Switch on the LED if an 1 was received as first character
   if ((char)payload[0] == '1')
   {
-    digitalWrite(LED_BUILTIN, LOW); // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is active low on the ESP-01)
+    digitalWrite(LED_BUILTIN, LOW);
   }
   else
   {
@@ -56,28 +50,20 @@ void log(char logmsg)
 
 void reconnect()
 {
-  // Loop until we're reconnected
-  while (!client.connected())
+   while (!client.connected())
   {
     Serial.print("Attempting MQTT connection...");
-    // Create a random client ID
     String clientId = "ESP8266Client-";
     clientId += String(random(0xffff), HEX);
-    // Attempt to connect
     if (client.connect(clientId.c_str()))
     {
-      //     log("connected");
-      // Once connected, publish an announcement...
       client.publish("outTopic", "hello world");
-      // ... and resubscribe
       client.subscribe("inTopic");
     }
     else
     {
       Serial.print("failed, rc=");
       Serial.print(client.state());
-      //    log(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
       delay(5000);
     }
   }
@@ -87,11 +73,9 @@ void setup()
 {
   Serial.begin(115200);
 
-  /* switch on led */
   pinMode(led_pin, OUTPUT);
   digitalWrite(led_pin, LOW);
 
-  //  log("Booting");
   WiFi.mode(WIFI_STA);
 
   WiFi.begin(ssid, password);
@@ -99,12 +83,11 @@ void setup()
   while (WiFi.waitForConnectResult() != WL_CONNECTED)
   {
     WiFi.begin(ssid, password);
-    //  log("Retrying connection...");
   }
-  /* switch off led */
+
   digitalWrite(LED_BUILTIN, HIGH);
 
-  /* configure dimmers, and OTA server events */
+
   analogWriteRange(1000);
   analogWrite(LED_BUILTIN, 990);
 
@@ -115,7 +98,7 @@ void setup()
   }
 
   ArduinoOTA.setHostname(host);
-  ArduinoOTA.onStart([]() { // switch off all the PWMs during upgrade
+  ArduinoOTA.onStart([]() {
     for (int i = 0; i < N_DIMMERS; i++)
     {
       analogWrite(dimmer_pin[i], 0);
@@ -123,7 +106,7 @@ void setup()
     analogWrite(LED_BUILTIN, 0);
   });
 
-  ArduinoOTA.onEnd([]() { // do a fancy thing with our board led at end
+  ArduinoOTA.onEnd([]() {
     for (int i = 0; i < 30; i++)
     {
       analogWrite(LED_BUILTIN, (i * 100) % 1001);
@@ -136,12 +119,11 @@ void setup()
     (void)error;
     ESP.restart(); });
 
-  /* setup the OTA server */
+  
   ArduinoOTA.begin();
   wifiServer.begin();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
-  //  log("Ready");
 }
 
 int Update_Loop_Count = 0;
@@ -158,8 +140,6 @@ void loop()
 
       while (server.available() > 0)
       {
-        // char received = server.read();
-        // server.write(received);
         char received = server.read();
         server.write(received);
       }
@@ -168,20 +148,9 @@ void loop()
     }
 
     server.stop();
-    //   log("Client disconnected");
   }
 
-  //  if (!client.connected())
-  //  {
-  //    reconnect();
-  //  }
-
-  // if (Update_Loop_Count < 30000)
-  // {
-  //   delay(1);
   ArduinoOTA.handle();
-  //   Update_Loop_Count++;
-  // }
+}
 
-  // client.loop();
 }
